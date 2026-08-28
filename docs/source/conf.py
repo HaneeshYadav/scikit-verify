9d227b8f672d8ebc5de0bb979dcc8e2838cf2699
Haneesh Kilari <haneesh.kilari@icloud.com>
2026-08-28 18:49:50 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,3 +28,7 @@ suppress_warnings = ['myst.xref_missing']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_css_files = [
+    'custom.css',
+]
